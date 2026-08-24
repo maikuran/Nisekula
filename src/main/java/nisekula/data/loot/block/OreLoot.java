540dccc4cf5d7d8940b36496073aa16c204d4adf
maikuran <abyssalx@outlook.jp>
2026-08-24 10:17:57 +0900

    Refactor OreLoot to use Blocks for loot registration
    Updated OreLoot class to use Blocks for ore types and removed unused constants.

--- src/main/java/nisekula/data/loot/block/OreLoot.java
+++ src/main/java/nisekula/data/loot/block/OreLoot.java
@@ -1,47 +1,32 @@
-package nisekula.world.loot;
+package nisekula.world.block;
 
-import nisekula.world.block.Blocks;
-import nisekula.world.item.ItemRegistry;
-
+import nisekula.world.loot.LootSystem;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class OreLoot {
 
-    // --- 鉱石・主要ブロックのID定義例 ---
-    public static final int STONE_ORE          = 20;
-    public static final int IRON_ORE           = 21;
-    public static final int YELLOW_CRYSTAL_ORE = 22;
-    public static final int DIAMOND_ORE        = 23;
-    public static final int LUXALBSTONE        = 24; // 白光岩
-
     /**
-     * 各種鉱石ブロックのドロップテーブルを初期化・登録する
+     * 鉱石類および白光岩のドロップテーブルを初期化・登録する
      */
     public static void init() {
-        // 石鉱石 (破壊時に石の素材やブロック自体をドロップ)
-        LootSystem.registerBlockLoot(STONE_ORE, Arrays.asList(
-            new LootSystem.LootEntry(STONE_ORE, 1.0f, 1, 1)
-        ));
-
-        // 鉄鉱石
-        LootSystem.registerBlockLoot(IRON_ORE, Arrays.asList(
-            new LootSystem.LootEntry(IRON_ORE, 1.0f, 1, 1)
+        // 鉄鉱石 (破壊時に鉄鉱石ブロック自身、または将来の鉱石アイテムをドロップ)
+        LootSystem.registerBlockLoot(Blocks.IRON_ORE, Arrays.asList(
+            new LootSystem.LootEntry(Blocks.IRON_ORE, 1.0f, 1, 1)
         ));
 
         // イエロークリスタル鉱石
-        LootSystem.registerBlockLoot(YELLOW_CRYSTAL_ORE, Arrays.asList(
-            new LootSystem.LootEntry(YELLOW_CRYSTAL_ORE, 1.0f, 1, 1)
+        LootSystem.registerBlockLoot(Blocks.YELLOW_CRYSTAL_ORE, Arrays.asList(
+            new LootSystem.LootEntry(Blocks.YELLOW_CRYSTAL_ORE, 1.0f, 1, 1)
         ));
 
-        // ダイヤ鉱石
-        LootSystem.registerBlockLoot(DIAMOND_ORE, Arrays.asList(
-            new LootSystem.LootEntry(DIAMOND_ORE, 1.0f, 1, 1)
+        // ダイヤモンド鉱石
+        LootSystem.registerBlockLoot(Blocks.DIAMOND_ORE, Arrays.asList(
+            new LootSystem.LootEntry(Blocks.DIAMOND_ORE, 1.0f, 1, 1)
         ));
 
-        // 白光岩 (Luxalbstone) - 洞窟の要、ムカデも好むブロック
-        LootSystem.registerBlockLoot(LUXALBSTONE, Arrays.asList(
-            new LootSystem.LootEntry(LUXALBSTONE, 1.0f, 1, 1)
+        // 白光岩 (Luxalbstone)
+        LootSystem.registerBlockLoot(Blocks.LUXALBSTONE, Arrays.asList(
+            new LootSystem.LootEntry(Blocks.LUXALBSTONE, 1.0f, 1, 1)
         ));
     }
 }
